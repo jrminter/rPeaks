da91e3ccea9c1770750a574cec668e619ce8dd90
John Minter <jrminter@gmail.com>
2015-05-19 19:50:17 -0400

    bump to 0.0.30 some tweaks of FitPeakToGaussian

--- R/FitPeakToGaussian.R
+++ R/FitPeakToGaussian.R
@@ -15,42 +15,39 @@
 #' @examples
 #' # Not run
 FitPeakToGaussian <- function(lData, doPlot=FALSE, pTitle='Peak',xTitle='x', yTitle='y'){
-                                # extract the data we need
-                                dat      <- lData$peakData
-                                muEst    <- lData$muEst
-                                sigmaEst <- lData$sigmaEst
-                                htEst    <- lData$htEst
-                                x <- dat$x
-                                y <- dat$y
+  # extract the data we need
+  dat      <- lData$peakData
+  muEst    <- lData$muEst
+  sigmaEst <- lData$sigmaEst
+  htEst    <- lData$htEst
+  x <- dat$x
+  y <- dat$y
 
-                                (res <- nls(y ~ scale*exp(-0.5*(x-mu)^2/sigma^2),
-                                            start=c(mu=muEst, sigma=sigmaEst, scale=htEst)))
+  (res <- nls(y ~ scale*exp(-0.5*(x-mu)^2/sigma^2),
+              start=c(mu=muEst, sigma=sigmaEst, scale=htEst)))
 
-                                sum <- summary(res)
+  sum <- summary(res)
+  ht <- sum$coefficients[3]
+  mu <- sum$coefficients[1]
+  sigma <- sum$coefficients[2]
 
-                                ht <- sum$coefficients[3]
-                                mu <- sum$coefficients[1]
-                                sigma <- sum$coefficients[2]
-
-                                if(doPlot==TRUE){
-                                  yc <- htEst*exp(-0.5*(x-muEst)^2/sigmaEst^2)
-                                  yc2 <- ht*exp(-0.5*(x-mu)^2/sigma^2)
-                                  xMin <- min(x)
-                                  xMax <- max(x)
-                                  yMax <- max(y)
-                                  plot(c(xMin, xMax), c(0, yMax), type='n',
-                                       xlab=xTitle, ylab=yTitle, main=pTitle)
-                                  points(x,y,pch=19)
-                                  lines(x,yc, col='red', lw=2)
-                                  lines(x,yc2, col='blue', lw=2)
-                                  legend("topright",
-                                         legend=c("points", "estimate", "fit"),
-                                         col=c('black', 'red', 'blue'),
-                                         pch=c(19, NA, NA),
-                                         lwd=c(NULL, 1,1))
-                                }
-
-                                # ret <- c(ht, mu, sigma)
-                                # names(ret) <- c("height", "mu", "sigma")
-                                return(sum)
-                              }
+  if(doPlot==TRUE){
+    yc <- htEst*exp(-0.5*(x-muEst)^2/sigmaEst^2)
+    yc2 <- ht*exp(-0.5*(x-mu)^2/sigma^2)
+    xMin <- min(x)
+    xMax <- max(x)
+    yMax <- max(y)
+    plot(c(xMin, xMax), c(0, yMax), type='n',
+         xlab=xTitle, ylab=yTitle, main=pTitle)
+         points(x,y,pch=19)
+         lines(x,yc, col='red', lw=2)
+         lines(x,yc2, col='blue', lw=2)
+         legend("topright", bty="n",
+         legend=c("points", "estimate", "fit"),
+         col=c('black', 'red', 'blue'),
+         lty=c(0, 1, 1),
+         lwd=c(0, 1,1),
+         pch=c(19, NA, NA))
+  }
+  return(sum)
+  }
